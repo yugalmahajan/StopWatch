56dd1a6bd1eba36007277f60bd714d6b3ac4d3d7
yugalmahajan <yugalmahajan1993@gmail.com>
2017-04-03 16:34:51 +0530

    Hello

---
+++ index.ios.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+
+import Main from './Main'
+import LapComponent from './Components/lapComponent'
+import tabBar from './tabBar'
+
+AppRegistry.registerComponent('StopWatch', () => tabBar);
